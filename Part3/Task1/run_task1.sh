f25ea44709f9a6d7303ce0a9479ea208b570c511
Souvik Roy <63651333+sroy0211@users.noreply.github.com>
2025-02-03 16:20:02 -0600

    Update run_task1.sh

--- Part3/Task1/run_task1.sh
+++ Part3/Task1/run_task1.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# The overall command for running the run_task1.sh is below:-
+# sudo docker exec master spark-submit <python file> <hdfs path for the dataset> <output folder name>
+
+# The sample command is below:-
 sudo docker exec master spark-submit task1.py hdfs://nn:9000/data1/ part_3_task_1
