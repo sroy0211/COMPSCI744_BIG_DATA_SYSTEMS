1983a3df15a9491c2dcd48b9fd567b82c8109964
Souvik Roy <63651333+sroy0211@users.noreply.github.com>
2025-02-02 19:01:49 -0600

    Add files via upload

---
+++ Part3/Task2/task2.py
@@ -0,0 +1,63 @@
+# Importing the necessary libraries
+import time
+import re
+import sys
+from operator import add
+from pyspark.sql import SparkSession
+
+# The function to compute rank contributions of a page to its linked neighbors
+def compute_probs(pages, rank):
+    num_pages = len(pages)
+    for page in pages:
+        yield (page, rank / num_pages)
+
+# The function to parse a line into (page, neighbor) tuples
+def parse_neighbors(pages):
+    neighbors = re.split(r'\s+', pages)
+    return neighbors[0], neighbors[1]
+
+# The main function to execute the PageRank algorithm
+def run_pagerank(input_path, output_path, num_partitions=8, num_iters=10):
+    spark = (SparkSession.
+            builder.
+            appName("PageRank").
+            getOrCreate())
+    
+     # Loading input files
+    lines = spark.read.text(input_path).rdd.map(lambda r: r[0])
+
+     # Reading the pages in input files and initializing their neighbors
+    links = lines.map(lambda pages: parse_neighbors(pages)).distinct().groupByKey()
+    
+    # Repartitioning the links
+    links = links.repartition(num_partitions)
+
+     # Initializing the ranks
+    ranks = links.map(lambda page_neighbors: (page_neighbors[0], 1.0)).repartition(num_partitions)
+
+    # Calculating and updating page ranks up to number of iterations
+    for iteration in range(num_iters):
+        # Calculating page contributions to the rank of other pages
+        probs = links.join(ranks).flatMap(
+                lambda page_pages_rank: compute_probs(page_pages_rank[1][0], page_pages_rank[1][1]))
+
+        # Re-calculates page ranks based on neighbor contributions
+        ranks = probs.reduceByKey(add).mapValues(lambda rank: rank * 0.85 + 0.15).repartition(num_partitions)
+        
+     # Writing the output
+    ranks.saveAsTextFile(output_path)
+
+    spark.stop()
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Usage: pagerank_partition.py <input> <output> <num_partition>")
+        sys.exit(-1)
+
+    num_partitions = sys.argv[3]
+    start = time.time()
+    run_pagerank(sys.argv[1], sys.argv[2], num_partitions=int(num_partitions))
+    end = time.time()
+    print("------------------------------")
+    print("Elapsed time: " + str(end - start) + " sec")
+    print("------------------------------")
