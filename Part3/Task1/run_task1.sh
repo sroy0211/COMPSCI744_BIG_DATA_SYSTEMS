74e11150edeafbb33260d994fb906e186f434e9b
Souvik Roy <63651333+sroy0211@users.noreply.github.com>
2025-02-04 17:30:55 -0600

    Update run_task1.sh

--- Part3/Task1/run_task1.sh
+++ Part3/Task1/run_task1.sh
@@ -4,4 +4,4 @@
 # sudo docker exec master spark-submit <python file> <hdfs path for the dataset> <output folder name>
 
 # The sample command is below:-
-sudo docker exec master spark-submit task1.py hdfs://nn:9000/data1/ part_3_task_1
+sudo docker exec master spark-submit task1.py hdfs://nn:9000/data1/ hdfs://nn:9000/part3_task1
