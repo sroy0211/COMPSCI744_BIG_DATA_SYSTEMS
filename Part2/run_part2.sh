07342acfc0f78004b8c5f9a2d8aa372d7634bc00
Souvik Roy <63651333+sroy0211@users.noreply.github.com>
2025-02-04 21:31:48 -0600

    Update run_part2.sh

--- Part2/run_part2.sh
+++ Part2/run_part2.sh
@@ -1,1 +1,7 @@
-sudo docker exec master spark-submit part2.py hdfs://nn:9000/data/ part2_output
+#!/bin/bash
+
+# The overall command for running the run_part2.sh is below:-
+# sudo docker exec master spark-submit <python file> <hdfs path for the dataset> <output folder name>
+
+# The sample command is below:-
+sudo docker exec master spark-submit part2.py hdfs://nn:9000/data/ hdfs://nn:9000/part2_output
