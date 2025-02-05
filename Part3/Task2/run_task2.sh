1f21d73d4d442b891c96dfdb3c3c20a94856c7fe
Souvik Roy <63651333+sroy0211@users.noreply.github.com>
2025-02-04 21:53:19 -0600

    Update run_task2.sh

--- Part3/Task2/run_task2.sh
+++ Part3/Task2/run_task2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # The overall command for running the run_task2.sh is below:-
-# sudo docker exec master spark-submit <python file> <hdfs path for the dataset> <output folder name> <number of partitions> <Host IP>
+# sudo docker exec master spark-submit <python file> <hdfs path for the dataset> <output folder name> <number of partitions> <memory_size in GB> <Host IP>
 
 # The sample command is below:-
 sudo docker exec master spark-submit task2.py hdfs://nn:9000/data1/ hdfs://nn:9000/part_3_task_2_data1_partition_1 1 3G hdfs://10.10.1.1:9000
