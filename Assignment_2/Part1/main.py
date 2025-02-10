03bdb16514fa42e433c1ed8c67073d37cc2eeb0a
kimayapatil <35213322+kimayapatil@users.noreply.github.com>
2025-02-09 19:07:08 -0600

    Update main.py

--- Assignment_2/Part1/main.py
+++ Assignment_2/Part1/main.py
@@ -1,3 +1,6 @@
+# Importing the necessary libraries
+
+import argparse
 import os
 import torch
 import json
@@ -10,25 +13,54 @@ import torch.optim as optim
 import logging
 import random
 import model as mdl
+import time
+
+# Setting the parameters
+
 device = "cpu"
 torch.set_num_threads(4)
+batch_size = 256 
+log_iter = 20
 
-batch_size = 256 # batch for one node
-def train_model(model, train_loader, optimizer, criterion, epoch):
+# Making the function for model training
+
+def train_model(model, train_loader, optimizer, criterion, epoch, stop_iter):
     """
     model (torch.nn.module): The model created to train
     train_loader (pytorch data loader): Training data loader
     optimizer (optimizer.*): A instance of some sort of optimizer, usually SGD
     criterion (nn.CrossEntropyLoss) : Loss function used to train the network
     epoch (int): Current epoch number
+    stop_iter (int): Stop iteration at the predetermined iteration
     """
+    start_time = time.time()
+    log_iter_start_time = time.time()
+    with open(f'output/{log_file_name}', 'a+') as f:
+        for batch_idx, (data, target) in enumerate(train_loader):
+            if batch_idx >= stop_iter:
+                break
+            batch_count = batch_idx + 1
+            data, target = data.to(device), target.to(device)
+            optimizer.zero_grad()
+            output = model(data)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
 
-    # remember to exit the train loop at end of the epoch
-    for batch_idx, (data, target) in enumerate(train_loader):
-        # Your code goes here!
-        break
+            elapsed_time = time.time() - start_time
 
+            f.write(f"{epoch},{batch_count},{elapsed_time}\n")
+
+            start_time = time.time()
+            if batch_count % log_iter == 0:
+                log_iter_elapsed_time = time.time() - log_iter_start_time
+                print('Train Epoch: {} \t Iteration: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\t elapsed time: {:.3f}'.format(
+                    epoch, batch_count, batch_count * len(data), len(train_loader.dataset),
+                    100. * batch_count / len(train_loader), loss.item(), log_iter_elapsed_time)) 
+                log_iter_start_time = time.time()
     return None
+
+# Making the function for testing the model
 
 def test_model(model, test_loader, criterion):
     model.eval()
@@ -46,9 +78,26 @@ def test_model(model, test_loader, criterion):
     print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
             test_loss, correct, len(test_loader.dataset),
             100. * correct / len(test_loader.dataset)))
-            
+
+# Completing the main function
 
 def main():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--master-ip', type=str, default='10.10.1.1', help='master node ip (default: 10.10.1.1)')
+    parser.add_argument('--num-nodes', type=int, default=1, help='the number of nodes (default:1)')
+    parser.add_argument('--rank', type=int, default=0, help='rank of node')
+    parser.add_argument('--epoch', type=int, default=1, help='the number of epochs (default:1)')
+    parser.add_argument('--stop_iter', type=int, default=40, help='Stop iteration at, (default: 40)')
+
+    args = parser.parse_args()
+    num_epochs = args.epoch
+    stop_iter = args.stop_iter
+    global log_file_name
+    log_file_name = f"timelog_{num_epochs}_{stop_iter}.csv"
+    with open(f'output/{log_file_name}', 'w+') as f:
+        f.write("epoch,iteration,elpased_time\n")
+
     normalize = transforms.Normalize(mean=[x/255.0 for x in [125.3, 123.0, 113.9]],
                                 std=[x/255.0 for x in [63.0, 62.1, 66.7]])
     transform_train = transforms.Compose([
@@ -83,10 +132,9 @@ def main():
     model.to(device)
     optimizer = optim.SGD(model.parameters(), lr=0.1,
                           momentum=0.9, weight_decay=0.0001)
-    # running training for one epoch
-    for epoch in range(1):
-        train_model(model, train_loader, optimizer, training_criterion, epoch)
-        test_model(model, test_loader, training_criterion)
+    for epoch in range(num_epochs):
+        train_model(model, train_loader, optimizer, training_criterion, epoch, stop_iter)
+    test_model(model, test_loader, training_criterion)
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     main()
